1f2b0669cc8331cd03abef2fca8b4f306065047a
blakejarryd <blakejarrydjohnston@gmail.com>
2022-07-08 13:32:01 +1000

    Added home page

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
+//acccess env variables
+require('dotenv').config()
+
 //NPM package dependancies
 const express = require('express')
 const mongoose = require("mongoose")
 const methodOverride = require('method-override')
 const session = require('express-session')
+require('dotenv').config()
 
 //Configuration
+PORT = process.env.PORT
 const app = express()
-const dbURL = 'mongodb://localhost:27017/userManagement'
-const PORT = 3000
+const dbURL = process.env.MONGODB_URL
+// const MongoDBStore = mongoDBSession(session)
+// const sessionStore = new MongoDBStore({
+//   uri: dbURL,
+//   collection: 'sessions'
+// })
 
 //Router
 const usersRouter = require('./src/routes/users.router')
 
 //Middleware
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
@@ -20,6 +30,11 @@ app.use('/users', usersRouter)
 
 //Config
 app.set('views', (__dirname + '/src/views'));
+
+//Homepage
+app.get('/', (req,res) => {
+  res.render('home.ejs')
+})
 
 //DB Connection
 mongoose.connect(dbURL, () => {
